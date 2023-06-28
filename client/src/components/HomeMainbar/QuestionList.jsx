d8931b57f0dd911e48fb361b915b730339083f86
vimal-oneway <vimalic555@gmail.com>
2023-06-28 09:39:05 +0530

    Hello world

--- client/src/components/HomeMainbar/QuestionList.jsx
+++ client/src/components/HomeMainbar/QuestionList.jsx
@@ -1,15 +1,14 @@
-import React from 'react'
-import Questions from './Questions'
-const QuestionList = ({questionsList}) => {
-    return (
-        <>
-            {
-                questionsList.map((question) => (
-                    <Questions question={question} key={question._id}/>
-                ))
-            }
-        </>
-    )
-}
+import React from "react";
+import Questions from "./Questions";
+const QuestionList = ({ questionsList }) => {
+  return (
+    <>
+      {questionsList &&
+        questionsList.map((question) => (
+          <Questions question={question} key={question._id} />
+        ))}
+    </>
+  );
+};
 
-export default QuestionList
+export default QuestionList;
